3b249f31b8e140cb0e6b28a25f539b2f03181c7d
Tony Xiao <tonyx.ca@gmail.com>
2017-12-02 20:56:24 -0800

    Allow QR code to use exp:// url

--- scripts/post-deploy.js
+++ scripts/post-deploy.js
@@ -3,17 +3,18 @@ const utils = require('./utils');
 const config = require('./config');
 const log = require('./log');
 module.exports = function postDeploy() {
-  const expUrl = `https://expo.io/@${config.expUsername}/${utils.readPackageJSON().name}`;
+  const expHttpUrl = `https://expo.io/@${config.expUsername}/${utils.readPackageJSON().name}`;
+  const expUrl = `exp://exp.host/@${config.expUsername}/${utils.readPackageJSON().name}`
   const qrUrl = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${expUrl}`;
   const issueUrl = `https://${config.githubUsername}:${config.githubToken}@api.github.com/repos/${config.githubOrg}/${config.githubRepo}/issues/${config.githubPullRequestId}/comments`;
 
-  log('Exponent URL', expUrl);
+  log('Exponent URL', expHttpUrl);
   log('GitHub Issue URL', issueUrl);
   log('QR Code URL ', qrUrl);
 
   const body = `
   :shipit: This branch has been deployed to:
-  ${expUrl}
+  ${expHttpUrl}
 
   Download the [Expo](https://expo.io/) app and scan this QR code to get started!
 
